f73cd1a4ddeea443289787d63c726ba0c719a1a0
cylcoder <cylcoder@gmail.com>
2026-02-24 22:12:24 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/bond/perseus/PerseusApplication.java
@@ -0,0 +1,13 @@
+package bond.perseus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerseusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PerseusApplication.class, args);
+  }
+
+}
---
+++ src/test/java/bond/perseus/PerseusApplicationTests.java
@@ -0,0 +1,13 @@
+package bond.perseus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseusApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
