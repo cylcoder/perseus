de4e434dcd0ea09cdb31b45b51df9f6168db6e07
cylcoder <cylcoder@gmail.com>
2026-02-26 21:55:53 +0900

    feat: 메일 발송 비동기 처리

--- src/main/java/bond/perseus/MailService.java
+++ src/main/java/bond/perseus/MailService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,7 @@ public class MailService {
 
   private final JavaMailSender mailSender;
 
+  @Async
   public void sendMail(String to, String subject, String text) {
     SimpleMailMessage message = new SimpleMailMessage();
     message.setFrom(username);
--- src/main/java/bond/perseus/PerseusApplication.java
+++ src/main/java/bond/perseus/PerseusApplication.java
@@ -2,8 +2,10 @@ package bond.perseus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PerseusApplication {
 
   public static void main(String[] args) {
